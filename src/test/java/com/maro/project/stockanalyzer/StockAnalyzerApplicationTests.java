f6590c273ae58ec0c455fb1edfb72f88f6e31dd5
김혜빈 <hyebin@eiz.local>
2025-08-20 22:54:26 +0900

    새 프로젝트 업로드

---
+++ src/test/java/com/maro/project/stockanalyzer/StockAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maro.project.stockanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
